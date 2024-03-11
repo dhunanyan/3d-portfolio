61ae62fb2e7d15d3504335dc4ebf9e932396d28e
dhunanyan <davit.hunanyan@me.com>
2024-03-11 21:04:59 +0100

    feat: add proper window animation

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -4,12 +4,13 @@ import * as React from "react";
 
 import "./LandingPage.scss";
 
+const ANIMATIONS_STEPS = ["step-1", "step-2"];
+
 export default function LandingPage() {
   return (
-    <div className="landing-page">
-      <div
-        className={`landing-page__wallpaper landing-page__wallpaper--blur`}
-      />
+    <div className={`landing-page`}>
+      <div className={`landing-page__wallpaper`} />
+      <div className={"landing-page__window"}></div>
     </div>
   );
 }
